f89e85f967f7b4236683a341ae785465e303be19
ArexTechnologies <68491090+ArexTechnologies@users.noreply.github.com>
2021-04-30 20:07:14 +0530

    Add files via upload

---
+++ precache-manifest.62bd2c533a18143d8dd83e1e8c9c2d6a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ab239c6d2fae9973914be29344f349cc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "53b049f3f7dba58e0509",
+    "url": "/static/css/2.8d3f7698.chunk.css"
+  },
+  {
+    "revision": "53b049f3f7dba58e0509",
+    "url": "/static/js/2.ebf85e95.chunk.js"
+  },
+  {
+    "revision": "384cab1b7cd6a44ee971731d66728117",
+    "url": "/static/js/2.ebf85e95.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9702bc1472adc8b40fc1",
+    "url": "/static/js/main.649f3c9b.chunk.js"
+  },
+  {
+    "revision": "992c1d3a7787c5ab29a5",
+    "url": "/static/js/runtime-main.eeaa320a.js"
+  },
+  {
+    "revision": "fdbca54c9e93271fe5e905330508d49c",
+    "url": "/static/media/potato.fdbca54c.jpeg"
+  }
+]);
